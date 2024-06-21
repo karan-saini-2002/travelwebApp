9ecd9102a3ed46b561ea3b0e53eedf1669d27c04
Karan Saini <115577838+karan-saini-2002@users.noreply.github.com>
2024-06-21 15:13:28 +0530

    fourth commmit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -15,25 +16,56 @@ app.use(cors());
 const mongoURI = process.env.MONGO_DB_URI;
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-// Package Schema
+const flightSchema = new mongoose.Schema({
+  details: String,
+  flightNumber: String,
+  departureDate: Date,
+  returnDate: Date
+});
+
+const hotelSchema = new mongoose.Schema({
+  details: String,
+  name: String,
+  address: String,
+  checkIn: Date,
+  checkOut: Date,
+  bookingDetails: String
+});
+
+const policySchema = new mongoose.Schema({
+  title: String,
+  description: String
+});
+
+const itineraryDaySchema = new mongoose.Schema({
+  day: Number,
+  date: Date,
+  hotel: String,
+  hotelStars: String,
+  car: String,
+  sightseeing: String
+});
+
 const packageSchema = new mongoose.Schema({
   destination: String,
   name: String,
   duration: String,
-  flights: String,
-  hotels: String,
+  flights: flightSchema,
+  hotels: hotelSchema,
   transfers: String,
-  activities: String,
+  activities: [String],
   meals: String,
   price: String,
   img: String,
+  imgUrls: [String],
+  policies: [policySchema],
+  itinerary: [itineraryDaySchema]
 });
-
 const Package = mongoose.model('Package', packageSchema);
 
 // Routes
@@ -47,6 +79,20 @@ app.get('/api/packages/:destination', async (req, res) => {
   }
 });
 
+app.get('/api/package/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const package = await Package.findById(id);
+    if (!package) {
+      return res.status(404).json({ message: 'Package not found' });
+    }
+    res.json(package);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
